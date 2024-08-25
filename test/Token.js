52c3880877b082e4e617cc664c36b91f2c047c74
dopau2011 <dopau2011@gmail.com>
2024-08-25 11:50:49 +0800

    6. Token approvals

--- test/Token.js
+++ test/Token.js
@@ -5,16 +5,17 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe('Token', () => {    //import hardhat
-  let token, accounts, deployer, receiver 
+describe('Token', () => {
+  let token, accounts, deployer, receiver, exchange
 
   beforeEach(async () => {
-    const Token = await ethers.getContractFactory('Token') 
+    const Token = await ethers.getContractFactory('Token')
     token = await Token.deploy('Dapp University', 'DAPP', '1000000')
 
     accounts = await ethers.getSigners()
     deployer = accounts[0]
     receiver = accounts[1]
+    exchange = accounts[2]
   })
 
   describe('Deployment', () => {
@@ -24,28 +25,28 @@ describe('Token', () => {    //import hardhat
     const totalSupply = tokens('1000000')
 
     it('has correct name', async () => {
-      expect(await token.name()).to.equal(name) 
+      expect(await token.name()).to.equal(name)
     })
-  
+
     it('has correct symbol', async () => {
-      expect(await token.symbol()).to.equal(symbol) 
+      expect(await token.symbol()).to.equal(symbol)
     })
-  
+
     it('has correct decimals', async () => {
-      expect(await token.decimals()).to.equal(decimals) 
+      expect(await token.decimals()).to.equal(decimals)
     })
-  
+
     it('has correct total supply', async () => {
-      expect(await token.totalSupply()).to.equal(totalSupply) 
+      expect(await token.totalSupply()).to.equal(totalSupply)
     })
 
     it('assigns total supply to deployer', async () => {
-      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)       
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
     })
 
   })
 
-  
+
   describe('Sending Tokens', () => {
     let amount, transaction, result
 
@@ -56,18 +57,18 @@ describe('Token', () => {    //import hardhat
         transaction = await token.connect(deployer).transfer(receiver.address, amount)
         result = await transaction.wait()
       })
-  
+
       it('transfers token balances', async () => {
         expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
         expect(await token.balanceOf(receiver.address)).to.equal(amount)
       })
-  
+
       it('emits a Transfer event', async () => {
         const event = result.events[0]
         expect(event.event).to.equal('Transfer')
-  
+
         const args = event.args
-        expect(args.form).to.equal(deployer.address)
+        expect(args.from).to.equal(deployer.address)
         expect(args.to).to.equal(receiver.address)
         expect(args.value).to.equal(amount)
       })
@@ -87,6 +88,40 @@ describe('Token', () => {    //import hardhat
 
     })
 
+  })
+
+  describe('Approving Token', () => {
+    let amount, transaction, result
+
+    beforeEach(async () => {
+      amount = tokens(100)
+      transaction = await token.connect(deployer).approve(exchange.address, amount)
+      result = await transaction.wait()
+    })
+
+    describe('Success', () =>{
+      it('allocates an allowance for delegated token spending', async () => {
+        expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
+      })
+
+      it('emits a Approval event', async () => {
+        const event = result.events[0]
+        expect(event.event).to.equal('Approval')
+
+        const args = event.args
+        expect(args.owner).to.equal(deployer.address)
+        expect(args.spender).to.equal(exchange.address)
+        expect(args.value).to.equal(amount)
+      })
+
+    })
+
+    describe('Failure', () => {
+      it('rejects invalid spenders', async () => {
+        await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+      })
+      
+    })
 
   })
 
